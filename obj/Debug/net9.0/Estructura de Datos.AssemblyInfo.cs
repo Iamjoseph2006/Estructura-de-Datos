09b600f7a60e52e47abf8b51e841f8b3677138c7
JOSE JHAIR HERNANDEZ TSEREMP <jj.hernandezt@uea.edu.ec>
2025-01-24 10:46:39 -0500

    Semana 6

--- obj/Debug/net9.0/Estructura de Datos.AssemblyInfo.cs
+++ obj/Debug/net9.0/Estructura de Datos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a2afa0c502976ac8672d470aa1dfbe9cc8a412")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24412b2baa9f4ae210880fcf35a7755c1bdd0554")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
